6413ba7158e9a2cb9d5add5e9b2991168a56706f
Anthony Jackson <anthony@jacksonct.com>
2023-07-31 09:27:59 -0400

    [AJ] Step1 - TDD Red - Initial failing unit test - not implemented exception

---
+++ DotNetCore/WC/WC/WCImplementation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WC
+{
+    public class WcImplementation
+    {
+        public int GetCharacterCount(string charactersToCount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DotNetCore/WC/WCTests/UnitTest1.cs
+++ DotNetCore/WC/WCTests/UnitTest1.cs
@@ -1,16 +1,25 @@
+using WC;
+
 namespace WCTests
 {
     public class Tests
     {
+        private readonly string _filePath = AppDomain.CurrentDomain.BaseDirectory + "Test.txt";
+        private string _stringToTest = "";
+        WcImplementation _wcImpl = new WcImplementation();
+
         [SetUp]
         public void Setup()
         {
+            _stringToTest = File.ReadAllText(_filePath);
+            
         }
 
         [Test]
-        public void Test1()
+        public void TestStep1_CharacterCount()
         {
-            Assert.Pass();
+            var characterCount = _wcImpl.GetCharacterCount(_stringToTest);
+            Assert.That(characterCount, Is.EqualTo(341836));
         }
     }
 }
